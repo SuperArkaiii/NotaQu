c9d116eac028b198afa516f52c0a832a63a98b07
Arkaiii <thisistdarx@gmail.com>
2025-07-29 08:00:31 +0700

    Brand Name = Komersial & Informasi

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,6 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandName('Komersial & Informasi')
             ->widgets([
                 SalesReport::class,
                 CostReport::class,
